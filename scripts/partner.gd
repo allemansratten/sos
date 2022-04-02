b082e3fff0a35276955c95aa26087fe24bb0dadf
Jkuzz <honza.ku@gmail.com>
2022-04-02 15:40:41 +0200

    Added basic movement interpolation

--- scripts/partner.gd
+++ scripts/partner.gd
@@ -11,30 +11,52 @@ var speed = STEP_SIZE
 var dir_x = 0
 var dir_y = 0
 
+var is_being_hit = false
+
 var colors = []
 var goal
 var patience = 2.0
+
+var delta_acc = 0
+var next_step_x = 0
+var next_step_y = 0
+var old_step_x = 0
+var old_step_y = 0
+
+
+func init(x, y, d_x=0, d_y=0):
+	print_debug("Spawning partner at [%d, %d], dir [%d, %d]" % [x, y, d_x, d_y])
+	position.x = x
+	old_step_x = x
+	next_step_x = x
+	
+	position.y = y
+	old_step_y = y
+	next_step_y = y
+	
+	dir_x = d_x
+	dir_y = d_y
+
 
 func random_color_choice(n_colors=2):
 	# force duplicate
 	var colors_tmp = ALL_COLORS + []
 	# clear up previous colors
 	colors = []
-	for i in range(n_colors):
+	for __ in range(n_colors):
 		var color_i = randi() % colors_tmp.size()
 		colors.append(colors_tmp.pop_at(color_i))
+
 
 func random_goal_choice():
 	goal = ALL_GOALS[randi() % ALL_GOALS.size()]
 
+
 func die():
 	modulate = Color("#904949")
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	random_color_choice(2)
-	random_goal_choice()
-	
+
+func make_flag(colors):
 	for c_i in range(len(colors)):
 		var flag = ColorRect.new()
 		flag.rect_position.x = -STEP_SIZE/2 + c_i*10
@@ -42,15 +64,23 @@ func _ready():
 		flag.color = ColorN(colors[c_i], 1)
 		flag.rect_size.x = 10
 		flag.rect_size.y = 10
-		add_child(flag)
-	
-var delta_acc = 0
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	random_color_choice(2)
+	random_goal_choice()
+	add_child(make_flag(colors))
+
 
 func _process(delta):
 	delta_acc += delta
 	if delta_acc >= SPEED_TIMESTEP:
 		delta_acc -= SPEED_TIMESTEP
 		_process_timestep()
+	
+	position.x = interpolate(old_step_x, next_step_x, delta_acc)
+	position.y = interpolate(old_step_y, next_step_y, delta_acc)
 	
 	# process patience
 	patience -= delta
@@ -59,10 +89,21 @@ func _process(delta):
 		get_parent().game_over("patience")
 	else:
 		pass
-		
+
+
+func interpolate(old_pos, new_pos, delta):
+	var normalised_delta = delta / SPEED_TIMESTEP
+	return old_pos + (old_pos - new_pos) * normalised_delta
+
+
 func _process_timestep():
-	position.x += dir_x * speed
-	position.y += dir_y * speed
+#	position.x += dir_x * speed
+#	position.y += dir_y * speed
+	old_step_x = position.x
+	old_step_y = position.y
+	next_step_x = position.x + dir_x * speed
+	next_step_y = position.y + dir_y * speed
+
 
 func check_color_intersect(other_colors):
 	for c in colors:
@@ -70,7 +111,7 @@ func check_color_intersect(other_colors):
 			return true
 	return false
 
-var is_being_hit = false
+
 func area_entered(other):
 	if other.is_in_group("partners"):
 		# Colliding with another partner
@@ -89,8 +130,9 @@ func area_entered(other):
 		other.is_being_hit = true
 	elif other.is_in_group("crossroads"):
 		set_direction(other.direction)
-		
+
 
 func set_direction(direction):
-	dir_x = [0, 1, 0, -1][direction]
-	dir_y = [-1, 0, 1, 0][direction]
+	#TODO: make this use Observer
+	dir_x = [0, -1, 0, 1][direction]
+	dir_y = [1, 0, -1, 0][direction]
--- scripts/partner_driver.gd
+++ scripts/partner_driver.gd
@@ -1,6 +1,7 @@
 extends Node
 
 export(PackedScene) var partner_scene
+#const Partner = preload("res://scenes/Partner.tscn")
 onready var next_partner_label = get_node("/root/GameScene/NextPartnerLabel")
 
 const SPAWN_DELAY = 6
@@ -14,23 +15,17 @@ func _ready():
 var partner_i = 0
 
 func spawn_partner():
-	print("spawning")
 	var partner = partner_scene.instance()
 	partner_i += 1
 	if partner_i == 1:
-		partner.dir_x = 1
-		partner.position.x = 64*1
-		partner.position.y = 64*4
+		partner.init(64*1, 64*4, -1, 0)
 	elif partner_i == 2:
-		partner.dir_y = -1
-		partner.position.x = 64*5
-		partner.position.y = 64*8
+		partner.init(64*5, 64*8, 0, 1)
 	else:
-		partner.position.y = 64*(1+randi()%4)
-		partner.dir_x = 1
-		
+		partner.init(64*1, 64*(1+randi()%4), -1, 0)
 	add_child(partner)
-	
+
+
 func game_over(reason):
 	# this relays the game_over call from partner
 	get_parent().game_over(reason)

22ae5614b8d0d32af400b5e277488b046f7917a4
Jkuzz <honza.ku@gmail.com>
2022-04-03 11:13:15 +0200

    Improved reliability of unreliable partners

--- scripts/crossroad_strategies.gd
+++ scripts/crossroad_strategies.gd
@@ -10,7 +10,11 @@ class UnreliableCrossroadStrategy:
 	func get_name():
 		return "unreliable"
 
-	func collide(crossroads, _in_direction):
-		# Pretends they're coming from a random direction
-		var dirs = [Vector2.UP, Vector2.DOWN, Vector2.LEFT, Vector2.RIGHT]
-		return crossroads.get_output_direction(dirs[randi() % 4])
+	func collide(crossroads, in_direction):
+		# Sometimes pretends they're coming from a random direction
+		var dir
+		if randi() % 10 == 0:
+			dir = [Vector2.UP, Vector2.DOWN, Vector2.LEFT, Vector2.RIGHT][randi() % 4]
+		else:
+			dir = in_direction
+		return crossroads.get_output_direction(dir)

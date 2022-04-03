86fb1685348fb103aef55c2f722b65ed1ec550f6
Vilém Zouhar <vilem.zouhar@gmail.com>
2022-04-03 18:43:52 +0200

    rework crossroads

--- scripts/all_crossroads.gd
+++ scripts/all_crossroads.gd
@@ -7,11 +7,10 @@ var game_phase = 0
 func _ready():
 	pass # Replace with function body.
 
-
 func get_phase():
 	return game_phase
 
-
 func inc_phase():
 	game_phase += 1
+	# TODO: set locked = false to unlock
 	get_tree().call_group("crossroads", "render_arrows")

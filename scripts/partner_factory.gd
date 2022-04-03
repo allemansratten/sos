7311e0ea994911cd8e2c752610e8af69adb6a3fa
Vilém Zouhar <vilem.zouhar@gmail.com>
2022-04-03 21:00:43 +0200

    purge allcrossroads

--- scripts/partner_factory.gd
+++ scripts/partner_factory.gd
@@ -3,14 +3,7 @@ class_name PartnerFactory
 extends Node
 export(PackedScene) var partner_scene
 
-onready var crossroads = get_node("/root/GameScene/AllCrossroads")
 onready var hud = get_node("/root/GameScene/HUD")
-
-var partners_spawned = 0
-
-func _ready():
-	pass
-
 
 func make_partner(location, partner_driver):
 	var partner_instance = partner_scene.instance()
@@ -23,8 +16,6 @@ func make_partner(location, partner_driver):
 		partner_driver,
 		partner_config
 	)
-	crossroads.inc_phase()
-	partners_spawned += 1
 	hud.update_total_partner(partners_spawned)
 	hud.send_pickup_line(partner_instance.partner_name)
 	return partner_instance

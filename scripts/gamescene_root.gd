19b6f76b35a6d38edcf457fc39f763b8243397a4
Vilém Zouhar <vilem.zouhar@gmail.com>
2022-04-03 20:34:02 +0200

    phases stub

--- scripts/gamescene_root.gd
+++ scripts/gamescene_root.gd
@@ -26,3 +26,16 @@ func game_over(reason, no_partners, location):
 	var label = hud.find_node("Label")
 	label.text = label.text.replace("REASON", reason).replace("NO_PARTNERS", no_partners)
 	add_child(hud)
+
+func refresh_phase(partner_count):
+	var to_unlock = []
+	if partner_count == 2:
+		get_node("FogOfWar/Phase1").queue_free()
+		to_unlock = get_tree().get_nodes_in_group("unlock_1")
+	elif partner_count == 10:
+		get_node("FogOfWar/Phase2").queue_free()
+		to_unlock = get_tree().get_nodes_in_group("unlock_2")
+		
+	for n in to_unlock:
+		print(n)
+		n.locked = false

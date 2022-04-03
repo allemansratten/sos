d59940a15ccb0fa89cf964b940e56c4fb409097f
Vilém Zouhar <vilem.zouhar@gmail.com>
2022-04-03 21:35:56 +0200

    legit goals

--- scripts/gamescene_root.gd
+++ scripts/gamescene_root.gd
@@ -2,6 +2,12 @@ extends Node
 
 export(PackedScene) var game_over_hud
 
+const GOALS_0 = ["cafe", "disco", "library"]
+const GOALS_1 = ["gallery"]
+const GOALS_2 = []
+const GOALS_3 = []
+var legit_goals = GOALS_0
+
 func _ready():
 	$MapCamera.make_current()
 	# randomize seeds generators
@@ -27,17 +33,29 @@ func game_over(reason, no_partners, location):
 	label.text = label.text.replace("REASON", reason).replace("NO_PARTNERS", no_partners)
 	add_child(hud)
 
+func get_phase(partner_count):
+	if partner_count >= 9:
+		return 3
+	elif partner_count >= 6:
+		return 2
+	elif partner_count >= 3:
+		return 1
+	return 0
+
 func refresh_phase(partner_count):
 	var to_unlock = []
-	if partner_count == 3:
+	if get_phase(partner_count) == 1:
 		get_node("FogOfWar/Phase1").queue_free()
 		to_unlock = get_tree().get_nodes_in_group("unlock_1")
-	elif partner_count == 6:
+		legit_goals += GOALS_1
+	elif get_phase(partner_count) == 2:
 		get_node("FogOfWar/Phase2").queue_free()
 		to_unlock = get_tree().get_nodes_in_group("unlock_2")
-	elif partner_count == 9:
+		legit_goals += GOALS_2
+	elif get_phase(partner_count) == 3:
 		get_node("FogOfWar/Phase3").queue_free()
 		to_unlock = get_tree().get_nodes_in_group("unlock_3")
+		legit_goals += GOALS_3
 		
 	for n in to_unlock:
 		print(n)

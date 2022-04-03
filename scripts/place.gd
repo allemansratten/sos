9b025a189b1fa8bb29fb7ced61ed516fb5b751bb
Václav Volhejn <vaclav.volhejn@gmail.com>
2022-04-03 20:29:04 +0200

    Play sound when place is entered

--- scripts/place.gd
+++
@@ -1,8 +0,0 @@
-extends Area2D
-
-export(String) var place
-
-func collide(partner):
-	if place == partner.goal:
-		print("satisfied partner's wish to go to ", place)
-		partner.schedule_random_goal_choice()

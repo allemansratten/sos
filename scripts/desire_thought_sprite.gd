888925ca7c4aaf76f13006422a22b8e018407042
Jkuzz <honza.ku@gmail.com>
2022-04-04 16:44:28 +0200

    Added files i forgot oops

---
+++ scripts/desire_thought_sprite.gd
@@ -0,0 +1,36 @@
+extends Sprite
+
+var low_patience = false
+var has_goal = true
+
+func _ready():
+	pass
+
+
+func _on_ThoughtTimer_timeout():
+	if low_patience:
+		self_modulate = Color(1, 0, 0)
+		self.show()
+	elif self.is_visible_in_tree():
+		self.hide()
+	elif has_goal:
+		self.show()
+
+
+# Show angry label when low patience
+func _on_Partner_low_patience():
+	low_patience = true
+	_on_ThoughtTimer_timeout()
+
+
+# Hide thoughts when goal is satisfied
+func _on_Partner_goal_satisfied():
+	has_goal = false
+	low_patience = false
+	self.hide()
+
+
+# When new goal is generated, display thought
+func _on_GoalRescheduleTimer_timeout():
+	has_goal = true
+	self.show()
